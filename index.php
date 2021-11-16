24f1bc368f1c45b4049cdfad81f53a9ab9073f41
Mat Jordan <mat@northwestern.edu>
2021-11-16 16:59:48 -0500

    Name plugin.

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+Plugin Name: Yith IIIF-WP
+Plugin URI: https://github.com/mathewjordan/yih-iiif-wp
+Description: Proof of concept for rendering of IIIF manifests in WordPress
+Version: 0.0.1
+Author: mat@northwestern.edu
+Author URI: https://github.com/mathewjordan
+*/
