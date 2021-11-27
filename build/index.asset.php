252c9a46e1ce48bb20230b865f8e64d6efab41e0
Mat Jordan <mat@northwestern.edu>
2021-11-27 10:58:40 -0500

    Remove hardcoding.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => 'a055b6fc04b1b2ec1ba6602493e830a6');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '64c6cbf7c630229db493f48f407b32f9');
--- src/save.js
+++ src/save.js
@@ -5,14 +5,13 @@ import { useBlockProps } from "@wordpress/block-editor";
 
 const Save = (props) => {
   const blockProps = useBlockProps.save();
-  console.log(props.attributes.preview);
   return (
     <>
       <div {...blockProps}>
         <div
           class="yith-iiif"
-          data-preview="interstitial"
-          data-text="interstitial"
+          data-preview={props.attributes.preview}
+          data-text={props.attributes.text}
           data-type={props.attributes.type}
         >
           <div class="yith-iiif-manifest" data-id={props.attributes.id}></div>
