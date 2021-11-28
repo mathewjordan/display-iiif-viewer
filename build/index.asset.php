1c751b72f4bc9cf0bba4342057cccae76d371051
Mat Jordan <mat@northwestern.edu>
2021-11-28 09:17:34 -0500

    Minimally style custom controls. :art:

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '64c6cbf7c630229db493f48f407b32f9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '40d5800fcaddb3185ddadfbe3281666a');
--- src/edit.js
+++ src/edit.js
@@ -9,11 +9,13 @@ import {
 } from "@wordpress/block-editor";
 import Yith from "@yith/yith";
 import * as Toolbar from "@radix-ui/react-toolbar";
+import { styled } from "@stitches/react";
 
 const Edit = (props) => {
   const blockProps = useBlockProps();
   const {
     attributes: { alignment, id, preview, type },
+    isSelected,
   } = props;
 
   const onChangeId = (newId) => {
@@ -38,6 +40,54 @@ const Edit = (props) => {
     });
   };
 
+  const Wrapper = styled(Toolbar.Root, {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#fff6",
+    padding: "0.25rem 0",
+    borderRadius: "0.5rem",
+    marginBottom: "1rem",
+  });
+
+  const Controls = styled("div", {
+    display: "flex",
+    flexDirection: "row",
+  });
+
+  const ControlGroup = styled(Toolbar.ToggleGroup, {
+    margin: "0 0.5rem",
+  });
+
+  const ControlItem = styled(Toolbar.ToggleItem, {
+    border: "none",
+    backgroundColor: "transparent",
+    fontFamily: "inherit",
+    fontSize: "1rem",
+    padding: "0.5rem",
+    margin: "0",
+
+    "&[data-state='on']": {
+      fontWeight: "700",
+    },
+  });
+
+  const Separator = styled(Toolbar.ToolbarSeparator, {
+    display: "flex",
+    width: "1px",
+    height: "70%",
+    backgroundColor: "#0003",
+    alignSelf: "center",
+  });
+
+  const Link = styled(Toolbar.Link, {
+    display: "flex",
+    border: "none",
+    backgroundColor: "transparent",
+    fontFamily: "inherit",
+    fontSize: "1rem",
+  });
+
   return (
     <div {...blockProps}>
       {
@@ -48,32 +98,30 @@ const Edit = (props) => {
           />
         </BlockControls>
       }
-      <Toolbar.Root>
-        <Toolbar.ToggleGroup
-          defaultValue="figure"
-          onValueChange={onChangePreview}
-          type="single"
-          value={preview}
-        >
-          <Toolbar.ToggleItem value="figure">Figure</Toolbar.ToggleItem>
-          <Toolbar.ToggleItem value="interstitial">
-            Interstitial
-          </Toolbar.ToggleItem>
-        </Toolbar.ToggleGroup>
-        <Toolbar.ToggleGroup
-          defaultValue="presentation"
-          onValueChange={onChangeType}
-          type="single"
-          value={type}
-        >
-          <Toolbar.ToggleItem value="presentation">
-            Presentation
-          </Toolbar.ToggleItem>
-          <Toolbar.ToggleItem value="projection">Projection</Toolbar.ToggleItem>
-        </Toolbar.ToggleGroup>
-        <Toolbar.Separator />
-        <Toolbar.Link>Docs</Toolbar.Link>
-      </Toolbar.Root>
+      <Wrapper>
+        <Controls>
+          <ControlGroup
+            defaultValue="figure"
+            onValueChange={onChangePreview}
+            type="single"
+            value={preview}
+          >
+            <ControlItem value="figure">Figure</ControlItem>
+            <ControlItem value="interstitial">Interstitial</ControlItem>
+          </ControlGroup>
+          <Separator />
+          <ControlGroup
+            defaultValue="presentation"
+            onValueChange={onChangeType}
+            type="single"
+            value={type}
+          >
+            <ControlItem value="presentation">Presentation</ControlItem>
+            <ControlItem value="projection">Projection</ControlItem>
+          </ControlGroup>
+        </Controls>
+        {/* <Link>Docs</Link> */}
+      </Wrapper>
 
       <URLInput
         disableSuggestions
@@ -87,8 +135,7 @@ const Edit = (props) => {
           <Yith.Manifest id={id} />
         </Yith>
       )}
-
-      <button>Add Manifest</button>
+      {/* {isSelected && <button>Add Manifest</button>} */}
     </div>
   );
 };
