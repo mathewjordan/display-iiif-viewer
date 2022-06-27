0605c06c84c279014347a951343f12cbcefe6070
Mat Jordan <mat@northwestern.edu>
2022-06-27 14:13:04 -0400

    Address absolute import of Yith on client side.

--- index.php
+++ index.php
@@ -14,9 +14,9 @@ defined( 'ABSPATH' ) || exit;
 /**
  * Load all translations for our plugin from the MO file.
 */
-add_action( 'init', 'yith_iiif_load_textdomain' );
+add_action( 'init', 'display_iiif_viewer_load_textdomain' );
 
-function yith_iiif_load_textdomain() {
+function display_iiif_viewer_load_textdomain() {
 	load_plugin_textdomain( 'display-iiif-viewer', false, basename( __DIR__ ) . '/languages' );
 }
 
@@ -26,28 +26,24 @@ function yith_iiif_load_textdomain() {
  *
  * Passes translations to JavaScript.
  */
-add_action( 'init', 'yith_iiif_wp_register_block' );
+add_action( 'init', 'display_iiif_viewer_register_block' );
 
-function yith_iiif_wp_register_block() {
-
+function display_iiif_viewer_register_block() {
 	// Register the block by passing the location of block.json.
-	register_block_type_from_metadata( 
-    __DIR__, 
-    // array( 'render_callback' => 'yith_iiif_wp_render_callback' ) 
-  );
+	register_block_type_from_metadata(__DIR__);
 
   if ( function_exists( 'wp_set_script_translations' ) ) {
     wp_set_script_translations( 'display-iiif-viewer', 'display-iiif-viewer' );
   }
-
-}
-
-function yith_iiif_wp_render_callback( $attributes ) {
-  print_r ($attributes);
-	return '<div class="new-dynamic-block">something</div>';
 }
 
 
 add_action('wp_enqueue_scripts', function () {
-  wp_enqueue_script('yith/iiif-wp', 'https://unpkg.com/@yith/yith@1.0.28/dist/yith.js', [], null, true);
+  wp_enqueue_script(
+    'yith/display-iiif-viewer',
+    plugin_dir_url( __FILE__ ) . 'build/index.js', 
+    ['wp-element'],
+    null,
+    true
+  );
 });
